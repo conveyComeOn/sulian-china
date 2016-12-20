1bcce094550bd1761b60f32567c8763e3bb9420e
conveyComeOn <351233603@qq.com>
2016-12-20 17:27:12 +0800

    首页改动

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ App({
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
+    //同步存入缓存 前面是key 后面是value
     wx.setStorageSync('logs', logs)
   },
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
+   
     }else{
+  
       //调用登录接口
       wx.login({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
               that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+           typeof cb == "function" && cb(that.globalData.userInfo)
+            
             }
           })
         }
