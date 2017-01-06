962ff492cdfd119202a630ac3d1d091686660a86
conveyComeOn <351233603@qq.com>
2017-01-06 11:50:35 +0800

    增加了发现-宿联
    增加并丰富了一些内容

--- pages/find/index.js
+++ pages/find/index.js
@@ -9,56 +9,56 @@ var app = getApp()
 Page({
   data: {
     lists: [
-      {
-        imgUrl: util+"http://photocdn.sohu.com/20160306/mp62095218_1457238787147_3.jpeg",
-        title: "台北特色民宿",
-        info: "高山流水，梅花三弄，夕阳箫鼓，汉宫秋月，阳春白雪，渔樵问答，胡笳十八拍，广陵散，平沙落雁，十面埋伏。",
+      // {
+      //   imgUrl: util+"http://photocdn.sohu.com/20160306/mp62095218_1457238787147_3.jpeg",
+      //   title: "台北特色民宿",
+      //   info: "高山流水，梅花三弄，夕阳箫鼓，汉宫秋月，阳春白雪，渔樵问答，胡笳十八拍，广陵散，平沙落雁，十面埋伏。",
        
-        uName: "张珊珊",
-        isVideo: true,
-        videoUrl: util.ossAliyuncs + "/videos/VID20161029121958.mp4",
-        browse: 13299,
-        like: 595,
-        comment: 789,
-        time: "刚刚"
-      },
-      {
-        imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_5.jpeg",
-        title: "女儿乡：台北旅店",
-        info: "九九八十一难，最难过的，其实是女儿国这一关，女儿乡的台北旅店，幽静与静谧的完美融合。",
+      //   uName: "张珊珊",
+      //   isVideo: true,
+      //   videoUrl: util.ossAliyuncs + "/videos/VID20161029121958.mp4",
+      //   browse: 13299,
+      //   like: 595,
+      //   comment: 789,
+      //   time: "刚刚"
+      // },
+      // {
+      //   imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_5.jpeg",
+      //   title: "女儿乡：台北旅店",
+      //   info: "九九八十一难，最难过的，其实是女儿国这一关，女儿乡的台北旅店，幽静与静谧的完美融合。",
        
-        uName: "麦田的守望者",
-        browse: 32499,
-        like: 222,
-        comment: 789,
-        time: "1小时前"
-      },
-      {
-        imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_6.jpeg",
-        title: "你好：台北旅店",
-        info: "位在苗栗标榜以人本与自然永续共生的生态建筑民宿「树也ChooArt Villa」，不仅本身就是一栋绿建筑，入住者更可在大片山林中呼吸著芬多精、欣赏著园区内种植的多种原生植物，并从屋顶花园观察到生物跳岛与廊道等生态造景，享受顶级且完整的生态之旅。",
+      //   uName: "麦田的守望者",
+      //   browse: 32499,
+      //   like: 222,
+      //   comment: 789,
+      //   time: "1小时前"
+      // },
+      // {
+      //   imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_6.jpeg",
+      //   title: "你好：台北旅店",
+      //   info: "位在苗栗标榜以人本与自然永续共生的生态建筑民宿「树也ChooArt Villa」，不仅本身就是一栋绿建筑，入住者更可在大片山林中呼吸著芬多精、欣赏著园区内种植的多种原生植物，并从屋顶花园观察到生物跳岛与廊道等生态造景，享受顶级且完整的生态之旅。",
         
-        uName: "雨碎江南",
-        isVideo: true,
-        videoUrl: util.ossAliyuncs + "/videos/%E7%8A%AF%E9%94%99-%E5%8F%8C%E7%AE%A1%E5%B7%B4%E4%B9%8C.mp4",
-        browse: 4299,
-        like: 2113,
-        comment: 789,
-        time: "昨天"
-      },
-      {
-        imgUrl: util+ "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_7.jpeg",
-        title: "南投「秋山居」",
-        info: "位在南投的「秋山居」特色在园区内的浓浓唐风以及大片玻璃窗美景，加上使用在地新鲜食材、工法细緻、摆盘诗意的无菜单佳餚，提供中国风的奢华享受。",
+      //   uName: "雨碎江南",
+      //   isVideo: true,
+      //   videoUrl: util.ossAliyuncs + "/videos/%E7%8A%AF%E9%94%99-%E5%8F%8C%E7%AE%A1%E5%B7%B4%E4%B9%8C.mp4",
+      //   browse: 4299,
+      //   like: 2113,
+      //   comment: 789,
+      //   time: "昨天"
+      // },
+      // {
+      //   imgUrl: util+ "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_7.jpeg",
+      //   title: "南投「秋山居」",
+      //   info: "位在南投的「秋山居」特色在园区内的浓浓唐风以及大片玻璃窗美景，加上使用在地新鲜食材、工法细緻、摆盘诗意的无菜单佳餚，提供中国风的奢华享受。",
        
-        uName: "小丸子",
-        isVideo: true,
-        videoUrl: util.ossAliyuncs + "/videos/%E8%BD%A6%E7%AB%99-%E5%8F%8C%E7%AE%A1%E5%B7%B4%E4%B9%8C.mp4",
-        browse: 3299,
-        like: 80,
-        comment: 352,
-        time: "2016-08-12"
-      },
+      //   uName: "小丸子",
+      //   isVideo: true,
+      //   videoUrl: util.ossAliyuncs + "/videos/%E8%BD%A6%E7%AB%99-%E5%8F%8C%E7%AE%A1%E5%B7%B4%E4%B9%8C.mp4",
+      //   browse: 3299,
+      //   like: 80,
+      //   comment: 352,
+      //   time: "2016-08-12"
+      // },
       {
         imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_8.jpeg",
         title: "屏东垦丁「船帆石866villa」",
@@ -108,9 +108,9 @@ Page({
 
 list:[
       {
-        imgUrl: util+"http://photocdn.sohu.com/20160306/mp62095218_1457238787147_3.jpeg",
+        imgUrl: util+"http://image.peoplearts.cn/Admin/Upload/Image/20160523/v_12294950477.jpg",
         title: "台北特色民宿",
-        info: "高山流水，梅花三弄，夕阳箫鼓，汉宫秋月，阳春白雪，渔樵问答，胡笳十八拍，广陵散，平沙落雁，十面埋伏。",
+        info: "高山流水，梅花三弄，夕阳箫鼓，汉宫秋月，阳春白。",
        
         uName: "张珊珊",
         isVideo: true,
@@ -121,9 +121,9 @@ list:[
         time: "刚刚"
       },
       {
-        imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_5.jpeg",
-        title: "女儿乡：台北旅店",
-        info: "九九八十一难，最难过的，其实是女儿国这一关，女儿乡的台北旅店，幽静与静谧的完美融合。",
+        imgUrl: util + "http://img.mp.itc.cn/upload/20161108/37794d2c7b8c431284645179bca0dcb2_th.jpeg",
+        title: "女儿乡:台北旅店",
+        info: "九九八十一难，最难过的，其实是女儿国这一关。",
        
         uName: "麦田的守望者",
         browse: 32499,
@@ -134,7 +134,7 @@ list:[
       {
         imgUrl: util + "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_6.jpeg",
         title: "你好：台北旅店",
-        info: "位在苗栗标榜以人本与自然永续共生的生态建筑民宿「树也ChooArt Villa」，不仅本身就是一栋绿建筑，入住者更可在大片山林中呼吸著芬多精、欣赏著园区内种植的多种原生植物，并从屋顶花园观察到生物跳岛与廊道等生态造景，享受顶级且完整的生态之旅。",
+        info: "位在苗栗标榜以人本与自然永续共生的生态建筑民宿",
         
         uName: "雨碎江南",
         isVideo: true,
@@ -147,7 +147,7 @@ list:[
       {
         imgUrl: util+ "http://photocdn.sohu.com/20160306/mp62095218_1457238787147_7.jpeg",
         title: "南投「秋山居」",
-        info: "位在南投的「秋山居」特色在园区内的浓浓唐风以及大片玻璃窗美景，加上使用在地新鲜食材、工法细緻、摆盘诗意的无菜单佳餚，提供中国风的奢华享受。",
+        info: "位在南投的「秋山居」特色在园区内的浓浓唐风以及大片玻璃窗美景",
        
         uName: "小丸子",
         isVideo: true,
@@ -211,8 +211,8 @@ list:[
   //进入详情
   detail: function (e) {
     console.log(e.currentTarget.dataset.title)
-    wx.navigateTo({
-      url: '../details/detail?id=' + e.currentTarget.dataset.id + '&title=' + e.currentTarget.dataset.title
-    })
+    // wx.navigateTo({
+    //   url: '../details/detail?id=' + e.currentTarget.dataset.id + '&title=' + e.currentTarget.dataset.title
+    // })
   }
 })
